1b3a6543e9d3e0839a6d3805cedd504cc9209d68
rodmoreira41 <a039291@ismai.pt>
2022-01-23 18:27:15 +0000

    gr

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host: 'localhost',
-    //host: 'mysql', NO DOCKER
+    //host: 'mysql', USAR ESTE HOST NO DOCKER
     user: 'root',
     password: '12345678',
     database: "biblioteca"
